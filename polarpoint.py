951b3d4fb3ad41d4cba65f93fd5d9f3a8d71c6f2
Antikit <antikit@yandex.ru>
2019-02-27 17:04:48 +0300

    NavCalcClass

--- polarpoint.py
+++ polarpoint.py
@@ -1,7 +1,7 @@
 
 class PolarPoint:
     """Class Polar Point.
-    Stores azimuth (in radians) and distance (in meters)"""
+    Stores azimuth (in degrees) and distance (in km)"""
     def __init__(self, azimuth=0.0, distance=0.0):
         self.__polar_point = {'azimuth': azimuth,
                               'distance': distance}
@@ -15,21 +15,21 @@ class PolarPoint:
                                   'distance': distance}
 
     def set_azimuth(self, azimuth):
-        """Adds azimuth (in radians) to polar point"""
+        """Adds azimuth (in degrees) to polar point"""
         if float(azimuth):
             self.__polar_point['azimuth'] = azimuth
 
     def set_distance(self, distance):
-        """Adds distance (in meters) to polar point"""
+        """Adds distance (in km) to polar point"""
         if float(distance):
             self.__polar_point['distance'] = distance
 
     def azimuth(self):
-        """Returns azimuth (in radians) of the polar point"""
+        """Returns azimuth (in degrees) of the polar point"""
         return self.__polar_point['azimuth']
 
     def distance(self):
-        """Returns distance (in meters) of the polar point"""
+        """Returns distance (in km) of the polar point"""
         return self.__polar_point['distance']
 
     def polar_coords(self):
